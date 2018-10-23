1e27c4b092b8ab0a4f5809b74a7295ab33a9be4d
juman <juman@172.17.204.11>
2018-10-23 21:20:47 +0900

    Task3

--- src/test/java/edu/handong/csee/pp1/dp/CLAMITest.java
+++ src/test/java/edu/handong/csee/pp1/dp/CLAMITest.java
@@ -60,10 +60,25 @@ public class CLAMITest {
 		// TASK3: Conduct defect prediction by using CLAMI models
 		// complete this test case for the generated arff files from Task 2
 		
-		//String[] args = {"-f","data/[your_generated_metric_data].arff","-l","label","-p", "buggy", "-m"};
+		// String[] args = {"-f","data/[your_generated_metric_data].arff","-l","label","-p", "buggy", "-m"};
 		
-		//CLAMI.main(args);
+		// String[] args = {"-f","data/spring-boot.arff","-l","label","-p", "buggy", "-m"};
+		String[] args = {"-f","data/techjobs-mvc.arff","-l","label","-p", "buggy", "-m"};
+		CLAMI.main(args);
 	}
+	
+	@Test
+	public void testCLAMIWithYourSelectedProject2() {
+		
+		// TASK3: Conduct defect prediction by using CLAMI models
+		// complete this test case for the generated arff files from Task 2
+		
+		// String[] args = {"-f","data/[your_generated_metric_data].arff","-l","label","-p", "buggy", "-m"};
+		
+		String[] args = {"-f","data/spring-boot.arff","-l","label","-p", "buggy", "-m"};
+		CLAMI.main(args);
+	}
+	
 	
 	@Test
 	public void testCLAMIWithApacheProjectToFindBestCutoffForPrecision() {
@@ -73,7 +88,7 @@ public class CLAMITest {
 		String bestCutoff = "50";
 		
 		String[] args = {"-f","data/Apache.arff","-l","isDefective","-p", "TRUE", "-m", "-c", bestCutoff};
-
+		
 		CLAMI.main(args);
 	}
 	
